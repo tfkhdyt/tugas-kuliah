cc23fc4f2a93c5e92386b6c28cc76b53b1a9a4d5
tfkhdyt <ta142fa@gmail.com>
2021-03-22 06:00:45 +0700

    nambah nomor 6

---
+++ Semester 2/Algoritma & Pemrograman 2 (Pak Yudi)/Tugas Halaman 264/tugas-algo-2-halaman-264-6.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class Jam {
+  static Scanner in = new Scanner(System.in);
+  
+  static void mengurangiJam(int jam, int menit, int detik, int p) {
+    int jamS,menitS,timeS, pS;
+    int jamNew = 0; int menitNew = 0; int detikNew = 0;
+    jamS = jam * 3600;
+    menitS = menit * 60;
+    pS = p * 60;
+    timeS = jamS + menitS + detik - pS;
+    if(timeS >= 3600){
+      jamNew = timeS / 3600;
+      menitNew = timeS % 3600 / 60;
+      detikNew = timeS % 3600 % 60;
+    }else{
+      menitNew = timeS / 60;
+      detikNew = timeS % 60;
+    }
+    
+    System.out.println("\n" + jam + ":" + menit + ":" + detik + " - " + p + " menit");
+    System.out.println("Hasil : "+ jamNew + ":" + menitNew + ":" + detikNew);
+  }
+
+  public static void main(String[] args) {
+    //Scanner in = new Scanner(System.in);
+    int jam, menit, detik, p;
+    System.out.print("Masukkan waktu (jam) : ");
+    jam = in.nextInt();
+    System.out.print("Masukkan waktu (menit) : ");
+    menit = in.nextInt();
+    System.out.print("Masukkan waktu (detik) : ");
+    detik = in.nextInt();
+    System.out.print("Masukkan nilai P (menit) : ");
+    p = in.nextInt();
+    
+    mengurangiJam(jam, menit, detik, p);
+  }
+}
