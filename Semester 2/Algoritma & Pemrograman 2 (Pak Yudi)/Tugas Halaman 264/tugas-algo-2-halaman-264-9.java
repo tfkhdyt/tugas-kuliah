ac499dddf840b8c184354fa5cf737da3f8f48ed7
tfkhdyt <ta142fa@gmail.com>
2021-03-24 07:04:55 +0700

    nambah soal nomor 9 dan 10

---
+++ Semester 2/Algoritma & Pemrograman 2 (Pak Yudi)/Tugas Halaman 264/tugas-algo-2-halaman-264-9.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.Period;
+
+public class TanggalBesok {
+   static void tanggalBesok(String date){
+      date = date.substring(6)+"-"+date.substring(3,5)+"-"+date.substring(0,2);
+      LocalDate tanggal = LocalDate.parse(date);
+      LocalDate yesterday = tanggal.minusDays(1);
+      String yesterdays = yesterday.toString();
+      yesterdays = yesterdays.substring(8,10)+"-"+yesterdays.substring(5,7)+"-"+yesterdays.substring(0,4);
+ 
+      System.out.println("\nKemarin adalah tanggal "+yesterdays);
+   }
+   public static void main(String args[]){
+      Scanner in = new Scanner(System.in);
+      System.out.print("Masukkan tanggal (dd-MM-yyyy): ");
+      String date = in.next();
+      tanggalBesok(date);
+   }
+}
