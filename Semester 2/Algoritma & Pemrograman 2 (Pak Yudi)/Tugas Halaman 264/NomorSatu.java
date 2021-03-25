e4375a7046c87e4b1a8e6c455ee36db401e46069
tfkhdyt <ta142fa@gmail.com>
2021-03-25 19:38:21 +0700

    nambah error handling pada nomor 1

--- Semester 2/Algoritma & Pemrograman 2 (Pak Yudi)/Tugas Halaman 264/NomorSatu.java
+++ Semester 2/Algoritma & Pemrograman 2 (Pak Yudi)/Tugas Halaman 264/NomorSatu.java
@@ -1,27 +1,28 @@
 import java.util.Scanner;
-
- // Compiler version JDK 11.0.2
-
- class NomorSatu
- {
-   static void hitungJumlah(int n){
+class NomorSatu {
+  static void hitungJumlah(int n) {
     int genap = 0;
     int j = 0;
     System.out.println();
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
       genap += j;
-      System.out.print(j +" ");
+      System.out.print(j + " ");
       j += 2;
     }
     System.out.println("\n\nJumlah " + n + " bilangan genap pertama adalah : " + genap);
-   }
-   
-   public static void main(String args[])
-   { 
+  }
+
+  public static void main(String args[]) {
     Scanner in = new Scanner(System.in);
-    System.out.print("Masukkan nilai N : ");
-    int n = in.nextInt();
-    
-    hitungJumlah(n);
-   }
- }
+    int n = 0;
+    try {
+      System.out.print("Masukkan nilai N : ");
+      n = in .nextInt();
+      hitungJumlah(n);
+    } catch (Exception ex) {
+      System.out.println("\nInput tidak valid, input harus bertipe integer (bilangan bulat)");
+    } finally {
+      in.close();
+    }
+  }
+}
