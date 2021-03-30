dec3a9b0ea121b6d2c8d42a392bfc1658a17440a
tfkhdyt <ta142fa@gmail.com>
2021-03-30 15:56:45 +0700

    fix nomor 11

--- Semester 2/Algoritma & Pemrograman 2 (Pak Yudi)/Tugas Halaman 338/NomorSebelas.java
+++ Semester 2/Algoritma & Pemrograman 2 (Pak Yudi)/Tugas Halaman 338/NomorSebelas.java
@@ -3,23 +3,18 @@ import java.util.Scanner;
 
 public class NomorSebelas {
   static Scanner in = new Scanner(System.in);
-  
-  static void same(int n){
+
+  static void same(int n) {
     int[] a = new int[n];
     for (int j = 0; j < n; j++) {
       System.out.print("a[" + j + "] = ");
       a[j] = in.nextInt();
     }
-    
-    
-    for(int i = 0;i < a.length;i++){
-      if(a[i] != a[i + 1]){
-        System.out.print("\nSeluruh isi array tidak sama");
-        break;
-      }else{
-        System.out.print("\nSeluruh isi array sama");
-        break;
-      }
+
+    if (a[0] != a[1]) {
+      System.out.print("\nSeluruh isi array tidak sama");
+    } else {
+      System.out.print("\nSeluruh isi array sama");
     }
   }
 
