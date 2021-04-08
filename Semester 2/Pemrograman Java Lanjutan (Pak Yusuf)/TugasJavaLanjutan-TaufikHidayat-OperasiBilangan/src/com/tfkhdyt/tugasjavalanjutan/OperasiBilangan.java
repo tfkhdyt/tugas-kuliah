68493b30c436cc446669e58fa39c1a786a3452ab
tfkhdyt <ta142fa@gmail.com>
2021-04-08 18:19:20 +0700

    nambah tugas javax swing operasi bilangan

--- Semester 2/Pemrograman Java Lanjutan (Pak Yusuf)/TugasJavaLanjutan-TaufikHidayat-OperasiBilangan/src/com/tfkhdyt/tugasjavalanjutan/OperasiBilangan.java
+++ Semester 2/Pemrograman Java Lanjutan (Pak Yusuf)/TugasJavaLanjutan-TaufikHidayat-OperasiBilangan/src/com/tfkhdyt/tugasjavalanjutan/OperasiBilangan.java
@@ -1,17 +1,6 @@
 package com.tfkhdyt.tugasjavalanjutan;
 import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author PC 6
- */
 public class OperasiBilangan extends javax.swing.JFrame {
 
     /**
