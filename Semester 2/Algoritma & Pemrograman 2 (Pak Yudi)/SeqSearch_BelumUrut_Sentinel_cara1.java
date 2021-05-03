655a6efca5ca4a64ab0f2d75196e8d719a2b0889
tfkhdyt <ta142fa@gmail.com>
2021-05-03 12:26:36 +0700

    nambah latihan search 2

--- Semester 2/Algoritma & Pemrograman 2 (Pak Yudi)/SeqSearch_BelumUrut_Sentinel_cara1.java
+++ Semester 2/Algoritma & Pemrograman 2 (Pak Yudi)/SeqSearch_BelumUrut_Sentinel_cara1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class SeqSearch_BelumUrut_Sentinel{
+class SeqSearch_BelumUrut_Sentinel_cara1{
   public static void main(String[] args){
     int[] x = {20,50,10,30,90,60,70,80,40,100,-1};
     Scanner in = new Scanner(System.in);
