0450a5f76cdba54172719968d322916b04c779ba
tfkhdyt <ta142fa@gmail.com>
2021-05-03 12:24:48 +0700

    nambah latihan search 2

---
+++ Semester 2/Algoritma & Pemrograman 2 (Pak Yudi)/SeqSearch_BelumUrut_Sentinel.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class SeqSearch_BelumUrut_Sentinel{
+  public static void main(String[] args){
+    int[] x = {20,50,10,30,90,60,70,80,40,100,-1};
+    Scanner in = new Scanner(System.in);
+    
+    System.out.print("Masukkan angka : ");
+    int y = in.nextInt();
+    
+    x[x.length - 1] = y;
+    
+    int i = 0;
+    while(x[i] != y)
+      i++;
+    if(i > x.length - 2)
+      System.out.println("Angka tidak ditemukan!");
+    else
+      System.out.println(y + " ditemukan! Berada pada elemen ke-" + i);
+    
+    in.close();
+  }
+}
