fc6e41f3ed405a71176f7e888522b50f764b65b6
tfkhdyt <ta142fa@gmail.com>
2021-05-03 14:08:34 +0700

    nambah latihan search 4 cara 1

--- Semester 2/Algoritma & Pemrograman 2 (Pak Yudi)/SeqSearch_Urut_Sentinel_cara1.java
+++ Semester 2/Algoritma & Pemrograman 2 (Pak Yudi)/SeqSearch_Urut_Sentinel_cara1.java
@@ -1,25 +1,22 @@
 import java.util.Scanner;
-class SeqSearch_BelumUrut_Sentinel_cara2{
+class SeqSearch_Urut_Sentinel_cara1{
   public static void main(String[] args){
     Scanner in = new Scanner(System.in);
     boolean found = false;
     
     System.out.print("Masukkan angka : ");
     int y = in.nextInt();
-    int[] x = {20,50,10,30,90,60,70,80,40,100,y};
+    int[] x = {10,20,30,40,50,60,70,80,90,100,y};
     
     int i = 0;
-    while(!found){
-      if(x[i] == y)
-        found = true;
-      else
-        i++;
-    }
-    
-    if(i == x.length - 1)
+    while(!found & x[i] < y)
+      i++;
+    if(i > x.length - 2)
       System.out.println("Angka tidak ditemukan!");
-    else
+    else if(x[i] == y)
       System.out.println(y + " ditemukan! Berada pada elemen ke-" + i);
+    else
+      System.out.println("Angka tidak ditemukan!");
     
     in.close();
   }
