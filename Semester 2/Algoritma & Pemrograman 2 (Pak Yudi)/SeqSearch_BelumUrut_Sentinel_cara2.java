8e5f812a67970c1f3736288455ab93c8d49830fe
tfkhdyt <ta142fa@gmail.com>
2021-05-03 12:35:33 +0700

    nambah latihan search 2 cara 2

--- Semester 2/Algoritma & Pemrograman 2 (Pak Yudi)/SeqSearch_BelumUrut_Sentinel_cara2.java
+++ Semester 2/Algoritma & Pemrograman 2 (Pak Yudi)/SeqSearch_BelumUrut_Sentinel_cara2.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
-class SeqSearch_BelumUrut_Sentinel_cara1{
+class SeqSearch_BelumUrut_Sentinel_cara2{
   public static void main(String[] args){
     int[] x = {20,50,10,30,90,60,70,80,40,100,0};
     Scanner in = new Scanner(System.in);
+    boolean found = false;
     
     System.out.print("Masukkan angka : ");
     int y = in.nextInt();
@@ -10,9 +11,14 @@ class SeqSearch_BelumUrut_Sentinel_cara1{
     x[x.length - 1] = y;
     
     int i = 0;
-    while(x[i] != y)
-      i++;
-    if(i > x.length - 2)
+    while(!found){
+      if(x[i] == y)
+        found = true;
+      else
+        i++;
+    }
+    
+    if(i == x.length - 1)
       System.out.println("Angka tidak ditemukan!");
     else
       System.out.println(y + " ditemukan! Berada pada elemen ke-" + i);
