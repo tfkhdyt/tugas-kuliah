08d1b62d689525c5d0f96d529118c3b7e0c28239
tfkhdyt <ta142fa@gmail.com>
2021-07-06 13:17:58 +0700

    nambah file negasi.java

---
+++ Semester 2/Logika Matematika (Pak Yudi)/negasi.java
@@ -0,0 +1,10 @@
+public class negasi{
+  public static void main (String[] args) {
+    int a = 13;
+    int b = 11;
+    boolean c = a > b;
+    
+    System.out.println(" p = " + a + " > " + b +" = " + c);
+    System.out.println("~p = " + a + " > " + b +" = " + !c);
+  }
+}
