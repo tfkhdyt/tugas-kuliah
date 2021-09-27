2d7e2a5d07f650c0b293051089c95f5b7ac3d4ef
tfkhdyt <ta142fa@gmail.com>
2021-09-27 11:19:35 +0700

    nambah tugas array struktur data semester 3

--- Semester 3/Struktur Data/tugas-array-1.cpp
+++ Semester 3/Struktur Data/tugas-array-1.cpp
@@ -1,8 +1,16 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main() {
-  int array[15] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024 2048, 4096, 8192, 16384};
+  string nama = "Taufik Hidayat";
+  string jurusan = "Teknik Informatika";
+  int array[15] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,15};
+  
+  printf("Nama       : %s\n", nama.c_str());
+  printf("Jurusan    : %s\n\n", jurusan.c_str());
   for(int i = 0; i < 15; i++){
-    printf("Nilai ke-%d: %d\n",i+1, array[i]);
+    printf("Nilai ke %2d: %2d\n",i+1, array[i]);
   }
+  
+  return 0;
 }
---
+++ Semester 3/Struktur Data/tugas-array-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  string nama = "Taufik Hidayat";
+  string jurusan = "Teknik Informatika";
+  int array[2][3] = {
+    {1, 2, 3},
+    {4, 5, 6}
+  };
+  
+  printf("Nama    : %s\n", nama.c_str());
+  printf("Jurusan : %s\n\n", jurusan.c_str());
+  for(int i = 0; i < 2; i++){
+    for(int j = 0; j < 3; j++){
+      printf("%d ", array[i][j]);
+    }
+    printf("\n");
+  }
+  return 0;
+}
