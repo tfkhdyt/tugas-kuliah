39bbd90cd299bd8765c01599ed02afc27532aa2c
tfkhdyt <ta142fa@gmail.com>
2021-10-04 14:11:23 +0700

    beresin semua fitur

--- Semester 3/Struktur Data/tugas-kelompok-array.cpp
+++ Semester 3/Struktur Data/tugas-kelompok-array.cpp
@@ -2,18 +2,24 @@
 #include <stdlib.h>
 using namespace std;
 
-void tampilGaris(){
+bool arraySudahTerisi = false;
+
+void clear() {
+  system("clear");
+}
+
+void tampilGaris() {
   printf("-------------------------------------------\n");
 }
 
-void tampilNama(){
-  printf("Nama Anggota : - Taufik Hidayat (301200032)\n");
-  printf("               - Teja Kusumah   (301200033)\n");
+void tampilNama() {
+  printf("Nama Anggota : Taufik Hidayat (301200032)\n");
+  printf("               Teja Kusumah   (301200033)\n");
   printf("Kelas        : IF Pagi\n");
   tampilGaris();
 }
 
-void tampilMenu(){
+void tampilMenu() {
   printf("1. Masukkan nilai\n");
   printf("2. Lihat array\n");
   printf("3. Hapus elemen array\n");
@@ -21,50 +27,118 @@ void tampilMenu(){
   printf("5. Exit\n");
 }
 
-int masukkanNilai(){
-  int jumlah;
+int masukkanNilai(int array[], int &jumlah) {
+  clear();
   printf("Masukkan jumlah elemen pada array (1 - 20): ");
   cin >> jumlah;
-  int array[jumlah];
-  for(int i = 0; i < jumlah; i++){
-    printf("\nMasukkan angka ke-%d: ", i + 1);
+  printf("\n");
+
+  for(int i = 0; i < jumlah; i++) {
+    printf("Masukkan angka ke-%d: ", i + 1);
     cin >> array[i];
-    printf("\n");
   }
+  arraySudahTerisi = true;
   return array[jumlah];
 }
 
-int main(){
-  int menu, array[20];
-  while(true){
+void tampilArray(int array[], int &jumlah) {
+  printf("Array: ");
+  if (jumlah == 0) printf("Kosong");
+  for(int i = 0; i < jumlah; i++) {
+    printf("%d", array[i]);
+    if (i < jumlah - 1) printf(", ");
+  }
+  printf("\n");
+  tampilGaris();
+}
+
+void hapusData(int array[], int &jumlah) {
+  int hapus;
+  printf("Masukkan data yang ingin dihapus: ");
+  cin >> hapus;
+
+  for (int i = 0; i < jumlah; i++) {
+    if (array[i] == hapus) {
+      for (int j = i; j < (jumlah - 1); j++) {
+        array[j] = array[j + 1];
+      }
+    }
+  }
+  jumlah--;
+}
+
+void reset(int array[], int &jumlah){
+  array = {};
+  jumlah = 0;
+}
+
+int main() {
+  int menu,
+  array[20] = {},
+  jumlah = 0;
+  while(true) {
     tampilNama();
     tampilMenu();
-    
     tampilGaris();
     printf("Pilih menu (1 - 5): ");
     cin >> menu;
-    
-    switch(menu){
+
+    switch(menu) {
       case 1:
-        system("clear");
-        array[20] = masukkanNilai();
-        printf("Input array sukses!\n");
-        for(int i = 0; i < sizeof(array)/sizeof(array[0]); i++){
-          printf("%d ", array[i]);
+        if (!arraySudahTerisi){
+          
+          masukkanNilai(array, jumlah);
+          clear();
+          printf("Input array sukses!\n");
+          tampilGaris();
+        } else {
+          clear();
+          printf("Array sudah terisi! Mohon reset terlebih dahulu\n");
+          tampilGaris();
         }
         break;
       case 2:
+        clear();
+        tampilArray(array, jumlah);
         break;
       case 3:
+        clear();
+        tampilArray(array, jumlah);
+        hapusData(array, jumlah);
+        clear();
+        printf("Data berhasil dihapus!\n");
+        tampilGaris();
+        tampilArray(array, jumlah);
         break;
       case 4:
+        char pilihan;
+        printf("Apakah Anda yakin ingin me-reset array (y/n)? ");
+        cin >> pilihan;
+        switch (pilihan) {
+          case 'y':
+            reset(array, jumlah);
+            clear();
+            printf("Array berhasil direset!\n");
+            tampilGaris();
+            break;
+          case 'n':
+            clear();
+            printf("Array batal direset!\n");
+            tampilGaris();
+            break;
+          default:
+            clear();
+            printf("Input tidak valid!\n");
+            tampilGaris();
+            break;
+        }
         break;
       case 5:
-        system("clear");
+        clear();
         printf("Anda telah keluar dari program!");
         return 0;
       default:
-        system("clear");
+        clear();
         printf("Input tidak valid! Pilih menu yang benar\n");
         tampilGaris();
         continue;
