7aa5160b6e31ad08f4b19b9314ee0e69d584dc60
tfkhdyt <ta142fa@gmail.com>
2021-10-25 11:57:38 +0700

    kelarin file descending-bubble-sort

--- Semester 3/Struktur Data/Descending Bubble Sort/descending-bubble-sort.cpp
+++ Semester 3/Struktur Data/Descending Bubble Sort/descending-bubble-sort.cpp
@@ -1,17 +1,57 @@
 #include <iostream>
 using namespace std;
 
-void cetakNama() {
+void cetakGaris() {
+  printf("========================================\n");
+}
+
+void cetakNama() { // function untuk menampilkan nama, nim, kelas, dan judul program
   printf(
-    "Nama          : Taufik Hidayat"
-    "NIM           : 301200032"
-    "Kelas         : IF Pagi"
-    "Judul Program : Descending Bubble Sort"
-    "======================================"
+    "Nama          : Taufik Hidayat\n"
+    "NIM           : 301200032\n"
+    "Kelas         : IF Pagi\n"
+    "Judul Program : Descending Bubble Sort\n"
   );
+  cetakGaris();
+}
+
+void tukar(int *a, int *b) { // function untuk menukar 2 buah data
+  int temp = *a; // masukkan alamat variabel a ke variabel temp
+  *a = *b; // masukkan alamat variabel b ke alamat variabel a
+  *b = temp; // masukkan variabel temp ke alamat variabel b
+}
+
+void urutkanArray(int array[], int jumlahIndex) { // function untuk mengurutkan array
+  for (int i = 1; i < jumlahIndex; i++) { // lakukan perulangan for sebanyak jumlah index dengan nilai awal 1
+    for (int j = jumlahIndex - 1; j >= i; j--) { // lakukan perulangan for ketika variabel j lebih dari sama dengan variabel i dengan nilai awal jumlah index - 1
+      if(array[j] > array[j-1]) { // apabila value array[j] lebih dari value array[j-1]
+        tukar(&array[j], &array[j-1]); // panggil function tukar dengan argumen &array[j] dan &array[j-1]
+      }
+    }
+  }
+}
+
+void cetakArray(int array[], int jumlahIndex) {
+  for (int i = 0; i < jumlahIndex; i++) {
+    printf("%d", array[i]);
+    if(i < jumlahIndex - 1) printf(", ");
+  }
 }
 
 int main() {
-  cetakNama();
+  int jumlahIndex, array[20]; // deklarasi variabel jumlahIndex dan array
+  cetakNama(); // memanggil function cetakNama()
+  printf("Masukkan jumlah index array (max 20) : "); // input jumlah index
+  cin >> jumlahIndex;
+  for (int i = 0; i < jumlahIndex; i++) { // input data array
+    printf("Masukkan data index ke-%d : ", i+1);
+    cin >> array[i];
+  }
+  cetakGaris();
+  printf("Array sebelum diurutkan:\n");
+  cetakArray(array, jumlahIndex);
+  urutkanArray(array, jumlahIndex); // panggil function urutkanArray dengan argumen array dan jumlahIndex
+  printf("\nArray sesudah diurutkan:\n");
+  cetakArray(array, jumlahIndex);
   return 0;
 }
