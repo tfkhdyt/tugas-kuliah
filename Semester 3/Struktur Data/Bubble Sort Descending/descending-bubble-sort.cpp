d53fdeae36cbad40e33a23488952c1ccecd30296
tfkhdyt <ta142fa@gmail.com>
2021-10-25 11:04:31 +0700

    nambah project bubble sort

---
+++ Semester 3/Struktur Data/Bubble Sort Descending/descending-bubble-sort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void cetakNama() {
+  printf(
+    "Nama          : Taufik Hidayat"
+    "NIM           : 301200032"
+    "Kelas         : IF Pagi"
+    "Judul Program : Descending Bubble Sort"
+    "======================================"
+  );
+}
+
+int main() {
+  cetakNama();
+  return 0;
+}
