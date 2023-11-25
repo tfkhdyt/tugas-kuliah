71b62ba10175c23a29f06bceecf5c74fa0ee6a29
tfkhdyt <me@tfkhdyt.my.id>
2023-11-25 09:21:43 +0700

    feat(neuro-computing): add UTS

---
+++ Semester 7/Neuro Computing/UTS/soal_no_1.m
@@ -0,0 +1,21 @@
+a = [
+  3 2 2;
+  0 -1 1;
+  3 1 1
+];
+b = [
+  4 4 1;
+  2 3 -1
+];
+c = [
+  0 5;
+  0 4;
+  1 2
+];
+
+soalA = 2 * a
+% soalB = a + b % Error
+% soalC = a * b % Error
+soalD = b * a
+soalE = (c * b)'
+soalF = a * eye(3)
---
+++ Semester 7/Neuro Computing/UTS/soal_no_3.m
@@ -0,0 +1,74 @@
+masukan = [
+    1 1 1;
+    1 1 -1;
+    1 -1 1;
+    -1 1 1
+];
+bias = [1 1 1 1];
+target = [1 1 1 1];
+
+perubahanBobot = [];
+for i = 1:size(masukan, 1)
+  deltaW1 = masukan(i, 1) * bias(i);
+  deltaW2 = masukan(i, 2) * bias(i);
+  deltaW3 = masukan(i, 3) * bias(i);
+  deltaB = target(i);
+
+  perubahanBobot(i, :) = [deltaW1 deltaW2 deltaW3 deltaB];
+end
+
+bobotBaru = [];
+inisialisasi = [0 0 0 0];
+for i = 1:size(perubahanBobot, 1)
+  w1 = perubahanBobot(i, 1) + inisialisasi(1);
+  w2 = perubahanBobot(i, 2) + inisialisasi(2);
+  w3 = perubahanBobot(i, 3) + inisialisasi(3);
+  b = perubahanBobot(i, 4) + inisialisasi(4);
+
+  inisialisasi = [w1 w2 w3 b];
+  bobotBaru(i, :) = inisialisasi;
+end
+
+net = [];
+for i = 1:size(masukan, 1)
+  n = (masukan(i, 1) * bobotBaru(4, 1)) + (masukan(i, 2) * bobotBaru(4, 2)) + (masukan(i, 3) * bobotBaru(4, 3)) + bobotBaru(4, 4);
+
+  net(i) = n;
+end
+
+for i = 1:4
+  fprintf('%d. Masukan:\n', i);
+  fprintf('   P1   = %d, ', masukan(i, 1));
+  fprintf('P2   = %d, ', masukan(i, 2));
+  fprintf('P3   = %d, ', masukan(i, 3));
+  fprintf('Bias = %d, ', bias(i));
+  fprintf('Target = %d\n', target(i));
+
+  fprintf('   Perubahan bobot:\n');
+  fprintf('   Δw1 = %d, ', perubahanBobot(i, 1));
+  fprintf('Δw2 = %d, ', perubahanBobot(i, 2));
+  fprintf('Δw3 = %d, ', perubahanBobot(i, 3));
+  fprintf('Δb  = %d\n', perubahanBobot(i, 4));
+
+  fprintf('   Bobot baru:\n');
+  fprintf('   w1 = %d, ', bobotBaru(i, 1));
+  fprintf('w2 = %d, ', bobotBaru(i, 2));
+  fprintf('w3 = %d, ', bobotBaru(i, 3));
+  fprintf('b  = %d\n', bobotBaru(i, 4));
+
+  fprintf('   Net = %d\n', net(i));
+
+  if net(i) >= 0
+    fn = 1;
+  else
+    fn = -1;
+  end
+  fprintf('   f(n) = %d\n', fn);
+
+  if fn == target(i)
+    valid = 'Valid';
+  else
+    valid = 'Tidak Valid';
+  end
+  fprintf('   Valid? = %s\n\n', valid);
+end
